86081815a9382ce1f7422089e520d77191389078
murat <yazilimgenel@gmail.com>
2023-11-26 19:43:37 +0100

    mac 26.11.23

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from 'react'
+import AppRouter from './router/AppRouter'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      App
+    <div>
+      <AppRouter/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
