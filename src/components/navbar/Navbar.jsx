2d76967f2e0f3251ce49d2bc46f16176b08f6e10
murat <yazilimgenel@gmail.com>
2023-12-03 13:59:48 +0100

    mac

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import {  Hamburger, Logo, Menu, MenuLink, Nav } from './NavbarStyles'
+import React from "react";
+import { Hamburger, Logo, Menu, MenuLink, Nav } from "./NavbarStyles";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useState } from 'react';
+import { useState } from "react";
 const Navbar = () => {
-
-  
+  const [navbarVisible, setNavBarVisible] = useState(false);
 
   return (
     <Nav>
@@ -13,17 +12,19 @@ const Navbar = () => {
         <span>recipe</span>
       </Logo>
 
-      <Hamburger>
+      <Hamburger onClick={() => setNavBarVisible(!navbarVisible)}>
         <GiHamburgerMenu />
       </Hamburger>
 
-      <Menu>
+      <Menu navBarState={navbarVisible} onClick={() => setNavBarVisible(false)}>
         <MenuLink to="/about"> about</MenuLink>
-        <a href="https://github.com/" target='blank'>github</a>
+        <a href="https://github.com/" target="blank">
+          github
+        </a>
         <MenuLink to="/">logout</MenuLink>
       </Menu>
     </Nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
